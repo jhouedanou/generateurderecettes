288c835d5e4ed44e33bafa5ca793d035d1cbd208
Jean Luc Houedanou <jeanluc@bigfiveabidjan.com>
2023-09-13 17:33:56 +0000

    header

--- php/index.php
+++ php/index.php
@@ -1,20 +1,35 @@
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Document</title>
 </head>
 <body>
     <?php 
+  // Check if the 'username' and 'email' keys are set in the $_GET array
+if (isset($_GET['username']) && isset($_GET['email'])) {
+    // Get the username and email from the $_GET array
+    $username = htmlspecialchars($_GET['username']);
+    $email = htmlspecialchars($_GET['email']);
 
-try {
-    $db = new PDO('mysql:host=db;dbname=recettes;charset=utf8', 'user', 'N7f68a&e69E+');
-    echo "Connected to database successfully!";
-} catch (PDOException $e) {
-    echo "Connection failed: " . $e->getMessage();
+    // Try to connect to the database if $email is not empty
+    if (!empty($email)) {
+        try {
+            $db = new PDO('mysql:host=db;dbname=recettes;charset=utf8', 'user', 'N7f68a&e69E+');
+            echo "Connected to database successfully!";
+        } catch (PDOException $e) {
+            echo "Connection failed: " . $e->getMessage();
+        }
+    } else {
+        // Redirect the user to the reserve.php page
+        //header('Location: accountCreation.php');
+        exit;
+    }
+} else {
+    // Redirect the user to the reserve.php page
+    //header('Location: accountCreation.php');
+    exit;
 }
 ?>
-</body>
-</html>
+<?php include('footer.php'); ?>
